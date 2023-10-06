91f7f3593b55310593d5a23a55112ce39f5f2a9c
misiriyathulhana <145666815+misiriyathulhana@users.noreply.github.com>
2023-10-06 16:13:05 +0530

    Add files via upload

---
+++ 6-10-23/count2.py
@@ -0,0 +1,9 @@
+list=['hana','sana','dina','safa']
+count=0
+a='a'
+print(list)
+for i in list:
+    for f in i:
+        if (f==a):
+            count=count+1
+print("the number of",a,"in",list,"is",count)
