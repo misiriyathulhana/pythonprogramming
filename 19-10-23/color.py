4c8f969c960eda95a4dce5c6ee6108a78b6f348c
misiriyathulhana <145666815+misiriyathulhana@users.noreply.github.com>
2023-10-27 15:44:48 +0530

    Delete 19-10-23/color.py

--- 19-10-23/color.py
+++
@@ -1,9 +0,0 @@
-color_list1=["white","black","red"]
-color_list2=["red","green"]
-print("orginal set elements")
-print(color_list1)
-print(color_list2)
-print("\n difference of color_list1 & color_list2")
-print(color_list1 - color_list2)
-print("\n difference of color_list2 & color_list1")
-print(color_list2 - color_list1)
